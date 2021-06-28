b9774f36a578c9f145be12e96988e17eacccffd6
Шмідт Олександр <sashashmidt02@gmail.com>
2021-06-28 19:10:38 +0300

    Создал Task и InputArrLenght для получения размера массива

--- Modul1HW4/Modul1HW4/Program.cs
+++ Modul1HW4/Modul1HW4/Program.cs
@@ -1,1 +1,21 @@
 ﻿using System;
+using System.Text;
+
+Task();
+
+void Task()
+{
+    InputArrLenght(out var lenght);
+    Console.WriteLine(lenght);
+}
+
+void InputArrLenght(out int lenght)
+{
+    Console.WriteLine("Array lenght: ");
+    var check = int.TryParse(Console.ReadLine(), out lenght);
+    while (!check)
+    {
+        Console.WriteLine("Wrong option. Try again: ");
+        check = int.TryParse(Console.ReadLine(), out lenght);
+    }
+}
